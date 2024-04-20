4bc720c10b682e54ce0466b262ec74826e41f4b1
shitalw20 <133356300+shitalw20@users.noreply.github.com>
2024-04-20 18:08:18 +0530

    added product make

--- application/views/sales/offer_register/vw_offer_register_update.php
+++ application/views/sales/offer_register/vw_offer_register_update.php
@@ -504,6 +504,14 @@ if (!$_SESSION['user_name']) {
 
 														</div>
 
+														<div class="btn-group" style="margin-top: 15px; margin-left: 20px;">
+															<?php $product_attachment = "demo_csv.csv"; ?>
+															<button style="width: 200px;  margin: auto;" type="button" class="btn btn-block btn-danger float-right">
+															<a  style="color: white;"  target="_blank" href="<?php echo base_url('assets/demo_csv.csv'); ?>">
+																Download Example Sheet</a>
+															</button>
+														</div>
+
 														<p style="color: #FF0000;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* Please Check HSN Code Of Product Before Submiting Offer</p>
 
 
